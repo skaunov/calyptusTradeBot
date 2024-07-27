b913f7edda75bcc3ecc3afc360a9e42cbc576f8d
skaunov <skaunov@disroot.org>
2024-07-27 13:30:35 +0300

    solution

--- src/index.ts
+++ src/index.ts
@@ -10,219 +10,155 @@ import {
 import * as phoenixSdk from "@ellipsis-labs/phoenix-sdk";
 
 export const execute = async () => {
-  const REFRESH_FREQUENCY_IN_MS = 2_000;
-  const MAX_ITERATIONS = 3;
+    const REFRESH_FREQ_MS = 2000;
+    const MAX_ITERATIONS = 1000;
+    const ORDER_LIFETIME_IN_SECONDS = 7;
+    
+    const EDGE = 0.5;
+    let counter = 0;
 
-  // Maximum time an order is valid for
-  const ORDER_LIFETIME_IN_SECONDS = 7;
+    if (!process.env.PRIVATE_KEY) {
+        throw new Error("Missing PRIVATE_KEY in your .env file");
+    }
 
-  // Edge of $0.5
-  const EDGE = 0.5;
-  let counter = 0;
+    let privateKeyArray;
+    try {privateKeyArray = JSON.parse(process.env.PRIVATE_KEY);}
+    catch (error) {
+        throw new Error("Error parsing PRIVATE_KEY. Please make sure it is a stringified array");
+    }
 
-  if (!process.env.PRIVATE_KEY) {
-    throw new Error("Missing PRIVATE_KEY in your .env file");
-  }
+    let traderKeypair = Keypair.fromSecretKey(Uint8Array.from(privateKeyArray));
 
-  let privateKeyArray;
-  try {
-    privateKeyArray = JSON.parse(process.env.PRIVATE_KEY);
-  } catch (error) {
-    throw new Error(
-      "Error parsing PRIVATE_KEY. Please make sure it is a stringified array"
+    const marketPubkey  = new PublicKey( "4DoNfFBfF7UokCC2FQzriy7yHK6DY6NVdYpuekQ5pRgg");
+    const connection = new Connection("https://api.devnet.solana.com");
+
+    const client = await phoenixSdk.Client.create(connection);
+
+    const marketState = client.marketStates.get(marketPubkey.toString());
+    const marketData = marketState?.data;
+    if (!marketData) {
+        throw new Error("Market data not found");
+    }
+
+    const setupNewMakerIxs = await phoenixSdk.getMakerSetupInstructionsForMarket(
+        connection,    marketState, traderKeypair.publicKey
     );
-  }
+    if (setupNewMakerIxs.length !== 0) {
+        const setup = new Transaction().add(...setupNewMakerIxs);
+        const setupTxId = await sendAndConfirmTransaction(connection, setup, [traderKeypair], {
+            skipPreflight: true, commitment: "confirmed",
+        });
+        console.log(`Setup Tx Link: https://beta.solscan.io/tx/${setupTxId}`);
+    }
+    else {   console.log("No setup required. Continuing...");}
 
-  let traderKeypair = Keypair.fromSecretKey(new Uint8Array(privateKeyArray));
-
-  const marketPubkey = new PublicKey(
-    "4DoNfFBfF7UokCC2FQzriy7yHK6DY6NVdYpuekQ5pRgg"
-  );
-  const endpoint = "https://api.mainnet-beta.solana.com";
-  const connection = new Connection(endpoint);
-  console.log('Creating connection');
-
-  // Create a Phoenix Client
-  const client = await phoenixSdk.Client.create(connection);
-  console.log('Created phoenixSdk client');
-
-  // Get the market metadata for the market you wish to trade on
-  const marketState = client.marketStates.get(marketPubkey.toString());
-  const marketData = marketState?.data;
-
-  if (!marketData) {
-    throw new Error("Market data not found");
-  }
-  console.log(`marketData: ${marketData.toString()}`);
-
-  const setupNewMakerIxs = await phoenixSdk.getMakerSetupInstructionsForMarket(
-    connection,
-    marketState,
-    traderKeypair.publicKey
-  );
-  console.log(`setupNewMakerIxs length: ${setupNewMakerIxs.length}`);
-
-  if (setupNewMakerIxs.length !== 0) {
-    const setup = new Transaction().add(...setupNewMakerIxs);
-    const setupTxId = await sendAndConfirmTransaction(
-      connection,
-      setup,
-      [traderKeypair],
-      {
-        skipPreflight: true,
-        commitment: "confirmed",
-      }
-    );
-    console.log(`Setup Tx Link: https://beta.solscan.io/tx/${setupTxId}`);
-  } else {
-    console.log("No setup required. Continuing...");
-  }
-
-  do {
-    // Before quoting, we cancel all outstanding orders
-    console.log('Before quoting, we cancel all outstanding orders');
-    const cancelAll = client.createCancelAllOrdersInstruction(
-      marketPubkey.toString(),
-      traderKeypair.publicKey
-    );
-    // Note we could bundle this with the place order transaction below, but we choose to cancel
-    // separately since getting the price could take a non-deterministic amount of time
-    try {
-      const cancelTransaction = new Transaction().add(cancelAll);
-      console.log('Send cancellation request');
-      const txid = await sendAndConfirmTransaction(
-        connection,
-        cancelTransaction,
-        [traderKeypair],
-        {
-          skipPreflight: true,
-          commitment: "confirmed",
+    do {
+        const cancelAll = client.createCancelAllOrdersInstruction(
+            marketPubkey.toString(), traderKeypair.publicKey,
+        );
+        try {
+            const cancelTx = new Transaction().add(cancelAll);
+            const cancelTxId = await sendAndConfirmTransaction(connection, cancelTx, [traderKeypair], {
+                skipPreflight: true, commitment: "confirmed",
+            });
+            console.log("Cancel tx link: https://beta.solscan.io/tx/" + cancelTxId);
         }
-      );
+        catch (err) {     console.log("Error: ", err);    }
+        
+        try {
+            const response = await fetch(
+                "https://api.coinbase.com/v2/prices/SOL-USD/spot"
+            );
+            const data/* : any */ = await response.json();
+            
+            if (!response.ok)
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            if (!data.data || !data.data.amount)
+              throw new Error("Invalid response structure");
+            
+            const price = data.data.amount;
 
-      console.log("Cancel tx link: https://beta.solscan.io/tx/" + txid);
-    } catch (err) {
-      console.log("Error: ", err);
-      continue;
-    }
+            let bidPrice = price - EDGE;
+            let askPrice = price + EDGE;
+    
+            console.log(`$SOL price: ${price}`);
+            console.log(`Placing bid (buy) order at: ${bidPrice}`);
+            console.log(`Placing ask (sell) order at: ${askPrice}`);
 
-    // Now we can place an order
-    console.log('Place order after the cancellation process');
+            const currentTiime = Math.floor(Date.now() / 1000);
+
+            const bidOrderTemplate: phoenixSdk.LimitOrderTemplate = {
+                side: phoenixSdk.Side.Bid,
+                priceAsFloat: bidPrice,
+                sizeInBaseUnits: 1,
+                selfTradeBehavior: phoenixSdk.SelfTradeBehavior.Abort,
+                clientOrderId: 1,
+                useOnlyDepositedFunds: true,
+                lastValidSlot: undefined,
+                lastValidUnixTimestampInSeconds: currentTiime + ORDER_LIFETIME_IN_SECONDS,
+            };
+            const bidLimitOrderIx = client.getLimitOrderInstructionfromTemplate(
+                marketPubkey.toBase58(), traderKeypair.publicKey, bidOrderTemplate
+            );
+
+            const askOrderTemplate: phoenixSdk.LimitOrderTemplate = {
+                side: phoenixSdk.Side.Ask,
+                priceAsFloat: askPrice,
+                sizeInBaseUnits: 1,
+                selfTradeBehavior: phoenixSdk.SelfTradeBehavior.Abort,
+                clientOrderId: 1,
+                useOnlyDepositedFunds: true,
+                lastValidSlot: undefined,
+                lastValidUnixTimestampInSeconds: currentTiime + ORDER_LIFETIME_IN_SECONDS,
+            };
+            const askLimitOrderIx = client.getLimitOrderInstructionfromTemplate(
+                marketPubkey.toBase58(), traderKeypair.publicKey, askOrderTemplate
+            );
+
+            // it's not *that* big problem that last orders will live for 7 seconds instead of 2
+            let ixs: TransactionInstruction[] = [bidLimitOrderIx, askLimitOrderIx];
+
+            try {
+                const placeQuotesTx = new Transaction().add(...ixs);
+                const placeQuotesTxId = await sendAndConfirmTransaction(connection, placeQuotesTx, [traderKeypair], {
+                    skipPreflight: true, commitment: "confirmed",
+                });
+                console.log(
+                    "Place quotes",
+                    bidPrice.toFixed(marketState.getPriceDecimalPlaces()),
+                    "@",
+                    askPrice.toFixed(marketState.getPriceDecimalPlaces()),
+                );
+                console.log(`Tx link: https://beta.solscan.io/tx/${placeQuotesTxId}`);
+            }
+            catch (err) {
+                console.log("Error: ", err);
+                continue;
+            }
+
+            counter++;
+            await delay(REFRESH_FREQ_MS);
+        } catch (error) {
+            console.error(error);
+        }
+    } while (counter < MAX_ITERATIONS);
+
+    const withdrawParams: phoenixSdk.WithdrawParams = {
+        quoteLotsToWithdraw: null, baseLotsToWithdraw: null
+    };
+    
     try {
-      // Get current SOL price from Coinbase
-      const response = await fetch(
-        "https://api.coinbase.com/v2/prices/SOL-USD/spot"
-      );
-      const data = await response.json();
-
-      if (!data.data || !data.data.amount)
-        throw new Error("Invalid response structure");
-
-      const price = parseFloat(data.data.amount);
-
-      let bidPrice = price - EDGE;
-      let askPrice = price + EDGE;
-
-      console.log(`SOL price: ${price}`);
-      console.log(`Placing bid (buy) order at: ${bidPrice}`);
-      console.log(`Placing ask (sell) order at: ${askPrice}`);
-
-      const currentTime = Math.floor(Date.now() / 1000);
-
-      const bidOrderTemplate: phoenixSdk.LimitOrderTemplate = {
-        side: phoenixSdk.Side.Bid,
-        priceAsFloat: bidPrice,
-        sizeInBaseUnits: 1,
-        selfTradeBehavior: phoenixSdk.SelfTradeBehavior.Abort,
-        clientOrderId: 1,
-        useOnlyDepositedFunds: false,
-        lastValidSlot: undefined,
-        lastValidUnixTimestampInSeconds:
-          currentTime + ORDER_LIFETIME_IN_SECONDS,
-      };
-
-      const bidLimitOrderIx = client.getLimitOrderInstructionfromTemplate(
-        marketPubkey.toBase58(),
-        traderKeypair.publicKey,
-        bidOrderTemplate
-      );
-
-      const askOrderTemplate: phoenixSdk.LimitOrderTemplate = {
-        side: phoenixSdk.Side.Ask,
-        priceAsFloat: askPrice,
-        sizeInBaseUnits: 1,
-        selfTradeBehavior: phoenixSdk.SelfTradeBehavior.Abort,
-        clientOrderId: 1,
-        useOnlyDepositedFunds: false,
-        lastValidSlot: undefined,
-        lastValidUnixTimestampInSeconds:
-          currentTime + ORDER_LIFETIME_IN_SECONDS,
-      };
-      const askLimitOrderIx = client.getLimitOrderInstructionfromTemplate(
-        marketPubkey.toBase58(),
-        traderKeypair.publicKey,
-        askOrderTemplate
-      );
-
-      let instructions: TransactionInstruction[] = [];
-      if (counter < MAX_ITERATIONS) {
-        instructions = [bidLimitOrderIx, askLimitOrderIx];
-      }
-
-      // If strategy has been executed for MAX_ITERATIONS times withdraw the funds from the exchange.
-      if (counter === MAX_ITERATIONS) {
-        // Create WithdrawParams. Setting params to null will withdraw all funds
-        const withdrawParams: phoenixSdk.WithdrawParams = {
-          quoteLotsToWithdraw: null,
-          baseLotsToWithdraw: null,
-        };
-
-        const placeWithdraw = client.createWithdrawFundsInstruction(
-          {
-            withdrawFundsParams: withdrawParams,
-          },
-          marketPubkey.toString(),
-          traderKeypair.publicKey
-        );
-        instructions.push(placeWithdraw);
-      }
-
-      // Send place orders/withdraw transaction
-      try {
-        const placeQuotesTx = new Transaction().add(...instructions);
-
-        const placeQuotesTxId = await sendAndConfirmTransaction(
-          connection,
-          placeQuotesTx,
-          [traderKeypair],
-          {
-            skipPreflight: true,
-            commitment: "confirmed",
-          }
-        );
-
-        console.log(
-          "Place quotes",
-          bidPrice.toFixed(marketState.getPriceDecimalPlaces()),
-          "@",
-          askPrice.toFixed(marketState.getPriceDecimalPlaces())
-        );
-        console.log(`Tx link: https://solscan.io/tx/${placeQuotesTxId}`);
-      } catch (err) {
-        console.log("Error: ", err);
-        continue;
-      }
-
-      counter += 1;
-      await delay(REFRESH_FREQUENCY_IN_MS);
-    } catch (error) {
-      console.error(error);
+        const withdrawTx = new Transaction().add(client.createCancelAllOrdersInstruction(
+            marketPubkey.toString(), traderKeypair.publicKey,
+        )).add(client.createWithdrawFundsInstruction(
+            {withdrawFundsParams: withdrawParams}, marketPubkey.toString(), traderKeypair.publicKey,
+        ));
+        const withdrawTxId = await sendAndConfirmTransaction(connection, withdrawTx, [traderKeypair], {
+            skipPreflight: true, commitment: "confirmed",
+        });
+        console.log("Cancel tx link: https://beta.solscan.io/tx/" + withdrawTx);
     }
-  } while (counter < MAX_ITERATIONS);
+    catch (err) {     console.log("Error: ", err);    }
 };
-
-export const delay = (time: number) => {
-  return new Promise<void>((resolve) => setTimeout(resolve, time));
-};
-
+export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 execute();
